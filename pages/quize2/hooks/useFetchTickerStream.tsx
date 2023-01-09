a78419576306b32b45c55ca65ea3446eda011885
watcharaphonjit <watcharaphonjit@gmail.com>
2023-01-09 21:45:52 +0700

    use arrow function instead

--- pages/quize2/hooks/useFetchTickerStream.tsx
+++ pages/quize2/hooks/useFetchTickerStream.tsx
@@ -24,13 +24,13 @@ const useFetchTickerStream = () => {
     });
   };
 
-  function sortByKey(array, key) {
+  const sortByKey = (array, key) => {
     return array.sort(function (a, b) {
       var x = a[key];
       var y = b[key];
       return x < y ? -1 : x > y ? 1 : 0;
     });
-  }
+  };
 
   useEffect(() => {
     connectWebSocket();
